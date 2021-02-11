7a81e400977a623e390566a03e6d85fa0c7060be
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-02-12 04:31:33 +0530

    fix: position

--- textual/Generate.py
+++ textual/Generate.py
@@ -17,7 +17,6 @@ def get_base_statement(move_from, move_to):
 
 def get_piece_statement(board: List[List[Box]], move_from: List[int], move_to: List[int]):
     return f"Move {board[move_from[0]][move_from[1]].get_piece().piece_type} to {get_chess_alpha_numeric(move_to[0], move_to[1])}"
-    pass
 
 
 def generate_textual_data(board: List[List[Box]], move_from: List[int], move_to: List[int]):

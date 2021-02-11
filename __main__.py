9a5e7e75e5120e1e277753e0f166fbaa602847d3
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-02-11 20:29:48 +0530

    feat: boilerplate for all available moves

--- __main__.py
+++ __main__.py
@@ -1,7 +1,10 @@
 """___main__.py"""
 from chess import Board
+from chess.Moves import get_all_available_moves
 
 if __name__ == '__main__':
     board = Board.init_board(Board.board)
     for i in range(8):
         print(board[i])
+
+    get_all_available_moves(board)
--- chess/Moves.py
+++ chess/Moves.py
@@ -4,10 +4,60 @@ from chess import Board
 from chess.Box import Box
 
 
+def get_pawn_moves() -> List[List[int]]:
+    list_of_moves = []
+    return list_of_moves
+
+
+def get_rook_moves() -> List[List[int]]:
+    list_of_moves = []
+    return list_of_moves
+
+
+def get_knight_moves() -> List[List[int]]:
+    list_of_moves = []
+    return list_of_moves
+
+
+def get_bishop_moves() -> List[List[int]]:
+    list_of_moves = []
+    return list_of_moves
+
+
+def get_queen_moves() -> List[List[int]]:
+    list_of_moves = []
+    return list_of_moves
+
+
+def get_king_moves() -> List[List[int]]:
+    list_of_moves = []
+    return list_of_moves
+
+
 def get_available_moves(board: List[List[Box]], i: int, j: int) -> List:
     list_of_moves = []
-    if board[i][j].is_occupied():
-        pass
+    box = board[i][j]
+    if box.is_occupied():
+        piece = box.get_piece()
+
+        if piece.piece_type == 'Pawn':
+            list_of_moves = get_pawn_moves()
+
+        if piece.piece_type == 'Rook':
+            list_of_moves = get_rook_moves()
+
+        if piece.piece_type == 'Knight':
+            list_of_moves = get_knight_moves()
+
+        if piece.piece_type == 'Bishop':
+            list_of_moves = get_bishop_moves()
+
+        if piece.piece_type == 'Queen':
+            list_of_moves = get_queen_moves()
+
+        if piece.piece_type == 'King':
+            list_of_moves = get_king_moves()
+
     return list_of_moves
 
 

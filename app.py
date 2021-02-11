38b011fb71c02c1cf4dd94fcb225afce037b6391
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-02-12 03:02:24 +0530

    feat: convert dict to box

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from typing import List
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
-from chess.Board import init_board
+from chess.Board import init_board, convert_box_to_dict
 from chess.Box import Box
 from chess.Moves import get_all_available_moves
+
+import json
 
 # from flask_cors import CORS
 
@@ -22,19 +24,19 @@ def index():
 
 @socketio.on('get available moves')
 def available_moves(data):
-    print('received data: ' + str(data))
+    board_dict = json.loads(str(data))
     # TODO: CONVERT data to python readable format.
     moves = get_all_available_moves(data)
     emit('available moves', moves)
 
 
 @socketio.on('connected')
-def handle_my_custom_event(json):
-    print('received json: ' + str(json))
-    board: List[List[Box]] = [[Box(i, j) for j in range(8)] for i in range(8)]
+def handle_my_custom_event(json_):
+    print('received json: ' + str(json_))
+    board = [[Box(i, j) for j in range(8)] for i in range(8)]
     board = init_board(board)
-    # TODO: Board Conversion
-    emit('init', board)
+    board = convert_box_to_dict(board)
+    emit('init', json.dumps(board))
 
 
 if __name__ == '__main__':

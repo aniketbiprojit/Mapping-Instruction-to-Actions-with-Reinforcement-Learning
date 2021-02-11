e55484653c55810aa42f218b09a568cf70c76615
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-02-12 03:29:01 +0530

    feat: add frontend

---
+++ frontend/src/Cell.ts
@@ -0,0 +1,47 @@
+export class Cell {
+	block: HTMLElement
+
+	piece_type: string
+	color: string
+
+	occupied = false
+	constructor(block: HTMLElement) {
+		this.block = block
+	}
+
+	update_from_server(board_elem) {
+		if (board_elem['occupied']) {
+			this.occupied = true
+			this.piece_type = board_elem['piece_type']
+			this.color = board_elem['color']
+
+			this.block.innerHTML = this.color.toLowerCase()[0] + this.piece_type[0]
+		} else {
+			this.occupied = false
+			this.piece_type = undefined
+			this.color = undefined
+			this.block.innerHTML = ''
+		}
+		this.block.setAttribute('data-occupied', this.occupied.toString())
+		this.block.setAttribute('data-piece_type', this.piece_type)
+		this.block.setAttribute('data-color', this.color)
+	}
+	// mark() {
+	// 	this.marked = true
+	// 	this.block.classList.add('marked')
+	// }
+
+	// unmark() {
+	// 	this.marked = false
+	// 	this.block.classList.remove('marked')
+	// }
+	// placeFood() {
+	// 	this.food = true
+	// 	this.block.classList.add('food')
+	// }
+
+	// eatFood() {
+	// 	this.food = false
+	// 	this.block.classList.remove('food')
+	// }
+}

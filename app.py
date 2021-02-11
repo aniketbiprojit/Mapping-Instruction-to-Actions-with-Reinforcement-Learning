79c92d355d498b3f28d7690bd22c89586c9841a9
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-02-12 02:40:12 +0530

    feat: flask inits

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+from typing import List
+
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
+
+from chess.Board import init_board
+from chess.Box import Box
+from chess.Moves import get_all_available_moves
 
 # from flask_cors import CORS
 
@@ -14,9 +20,21 @@ def index():
     return render_template("index.html")
 
 
-@socketio.on('my event')
+@socketio.on('get available moves')
+def available_moves(data):
+    print('received data: ' + str(data))
+    # TODO: CONVERT data to python readable format.
+    moves = get_all_available_moves(data)
+    emit('available moves', moves)
+
+
+@socketio.on('connected')
 def handle_my_custom_event(json):
     print('received json: ' + str(json))
+    board: List[List[Box]] = [[Box(i, j) for j in range(8)] for i in range(8)]
+    board = init_board(board)
+    # TODO: Board Conversion
+    emit('init', board)
 
 
 if __name__ == '__main__':

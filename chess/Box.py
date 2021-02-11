d9960ba5385cb97846622c84b594e281020f19f7
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-02-11 19:44:06 +0530

    feat: Box class

--- chess/Box.py
+++ chess/Box.py
@@ -4,8 +4,8 @@ from chess.Piece import Piece
 class Box:
     i: int = 0
     j: int = 0
-    occupied: bool = False
-    piece: Piece
+    __occupied: bool = False
+    __piece: Piece
 
     def __init__(self, i: int, j: int) -> None:
         self.i = i
@@ -16,8 +16,21 @@ class Box:
             return self.piece
         return self.piece
 
-    def set_piece(self):
-        pass
+    def set_piece(self, piece: Piece):
+        if self.occupied:
+            return False
+        self.occupied = True
+        self.piece = piece
+        return True
+
+    def remove_piece(self):
+        if self.occupied:
+            return True
+        self.piece = None
+        return True
+
+    def is_occupied(self):
+        return self.occupied
 
     def __repr__(self):
         return f'[{self.i}][{self.j}]'

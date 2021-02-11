bbea9f9532e45b9cfbb9e57323225969867b427e
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-02-11 22:51:09 +0530

    fix: python socket fixes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+
 # from flask_cors import CORS
 
 app = Flask(__name__, static_folder='./static', template_folder='./template')
@@ -12,9 +13,11 @@ socketio: SocketIO = SocketIO(app, cors_allowed_origins="*")
 def index():
     return render_template("index.html")
 
+
 @socketio.on('my event')
 def handle_my_custom_event(json):
     print('received json: ' + str(json))
+
 
 if __name__ == '__main__':
     socketio.run(app)

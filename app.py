d0bedf176a81294c51f1899cae49e5f35418f181
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-02-11 21:29:32 +0530

    partial: sockets cors issue

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+# from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./static', template_folder='./template')
+# cors = CORS(app, resources={r"/*": {"origins": "*"}})
+# noinspection SpellCheckingInspection
+socketio: SocketIO = SocketIO(app, cors_allowed_origins="*")
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template("index.html")
 
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)

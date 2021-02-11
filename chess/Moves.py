6de849b37f752063f868f7d581fb60557b21624c
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-02-12 01:36:20 +0530

    feat: rook moves

--- chess/Moves.py
+++ chess/Moves.py
@@ -1,6 +1,5 @@
 from typing import List
 
-from chess import Board
 from chess.Box import Box
 
 
@@ -8,44 +7,80 @@ def is_a_box(i: int, j: int) -> bool:
     return (0 <= i < 8) and (0 <= j < 8)
 
 
-def is_capturable(board: List[List[Box]], i1, j1, i2, j2) -> bool:
+def filtered_for_is_a_box(available_locations):
+    return [elem for elem in available_locations if is_a_box(elem[0], elem[1])]
+
+
+def is_capturable(board: List[List[Box]], i1, j1, i2, j2, allow_empty: bool = True) -> bool:
     box_1 = board[i1][j1]
     box_2 = board[i2][j2]
     if not box_2.is_occupied():
-        return True
+        if allow_empty:
+            return True
+        else:
+            return False
     else:
         piece_1 = box_1.get_piece()
         piece_2 = box_2.get_piece()
         if piece_1 and piece_1.color != piece_2.color:
-            print(piece_1.color, piece_2.color)
+            # print(piece_1.color, piece_2.color)
             return True
     return False
 
 
-def get_pawn_moves(board: List[List[Box]], i: int, j: int) -> List[List[int]]:
+def get_pawn_moves(board: List[List[Box]], i: int, j: int) -> List[List[List[int]]]:
+    # TODO: Test capture
     list_of_moves = []
-    box = board[i][j]
-    piece = box.get_piece()
-    if piece and piece.piece_type == 'Pawn':
-        pass
+    available_locations = []
+    if board[i][j].get_piece() and board[i][j].get_piece().color == 'Black':
+        available_locations.append([i + 1, j])
+        if i == 1:
+            available_locations.append([i + 2, j])
+        if is_a_box(i + 1, j + 1) and is_capturable(board, i, j, i + 1, j + 1, allow_empty=False):
+            available_locations.append([i + 1, j + 1])
+
+        if is_a_box(i + 1, j - 1) and is_capturable(board, i, j, i + 1, j - 1, allow_empty=False):
+            available_locations.append([i + 1, j - 1])
+
+    if board[i][j].get_piece() and board[i][j].get_piece().color == 'White':
+        available_locations.append([i - 1, j])
+        if i == 6:
+            available_locations.append([i - 2, j])
+
+        if is_a_box(i - 1, j + 1) and is_capturable(board, i, j, i - 1, j + 1, allow_empty=False):
+            available_locations.append([i - 1, j + 1])
+
+        if is_a_box(i - 1, j - 1) and is_capturable(board, i, j, i - 1, j - 1, allow_empty=False):
+            available_locations.append([i - 1, j - 1])
+
+    available_locations = [elem for elem in available_locations if is_a_box(elem[0], elem[1])]
+    for location in available_locations:
+        list_of_moves.append([[i, j], location])
+
     return list_of_moves
 
 
 def get_rook_moves(board: List[List[Box]], i: int, j: int) -> List[List[int]]:
     list_of_moves = []
-    box = board[i][j]
-    piece = box.get_piece()
-    if piece and piece.piece_type == 'Rook':
-        pass
+
     return list_of_moves
 
 
-def get_knight_moves(board: List[List[Box]], i: int, j: int) -> List[List[int]]:
+def get_knight_moves(board: List[List[Box]], i: int, j: int) -> List[List[List[int]]]:
+    # TODO: Test Rook moves
     list_of_moves = []
-    box = board[i][j]
-    piece = box.get_piece()
-    if piece and piece.piece_type == 'Knight':
-        pass
+    l1 = [(1, 2), (2, 1)]
+    l2 = [(1, 1), (1, -1), (-1, -1), (-1, 1)]
+
+    available_locations = [[val[0] * elem[0] + i, val[1] * elem[1] + j] for val in l2 for elem in l1]
+    # print(i, j)
+
+    available_locations = filtered_for_is_a_box(available_locations)
+
+    for location in available_locations:
+        if is_capturable(board, i, j, location[0], location[1]):
+            list_of_moves.append([[i, j], location])
+
     return list_of_moves
 
 
@@ -71,8 +106,9 @@ def get_king_moves(board: List[List[Box]], i: int, j: int) -> List[List[int]]:
     # TODO: Test it
     list_of_moves = []
     available_locations = [[i + x, j + y] for x in range(-1, 2) for y in range(-1, 2) if (x, y) != (0, 0)]
-    available_locations = [elem for elem in available_locations if is_a_box(elem[0], elem[1])]
-    print(available_locations)
+
+    available_locations = filtered_for_is_a_box(available_locations)
+
     for location in available_locations:
         if is_capturable(board, i, j, location[0], location[1]):
             list_of_moves.append([[i, j], location])
